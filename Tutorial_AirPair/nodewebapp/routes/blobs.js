dc396ed3796abeaf71461cb9208dc600e67be3f6
lefreitas.54@gmail.com <lefreitas.54@gmail.com>
2016-09-15 15:22:37 -0300

    foi terminado o controller, o próximo passo é fazer as telas/paginas

--- Tutorial_AirPair/nodewebapp/routes/blobs.js
+++ Tutorial_AirPair/nodewebapp/routes/blobs.js
@@ -65,46 +65,70 @@ router.route('/')
 		})
 	});
 
-	router.get('/new', function(req, res){
-		res.render('blobs/new', {title: 'Add New Blob'});
-	});
+router.get('/new', function(req, res){
+	res.render('blobs/new', {title: 'Add New Blob'});
+});
 
-	router.params('id', function(req, res, next, id){
-		
-		mongoose.model('Blob').findById(id, function(err, blob){
-			if(err){
-				console.log(id + ' não encontrado');
-				res.status(404)
-				var err new Error('Não Encontrado');
-				err.status = 404;
-				res.format({
-					html: function(){
-						next(err);					
-					},
-					json: function(){
-						res.json({message: err.status + ' ' + err});
-					}
-				});
-			}else{
-				req.id = id;
-				next();
-			}
-		});
+router.params('id', function(req, res, next, id){
+	
+	mongoose.model('Blob').findById(id, function(err, blob){
+		if(err){
+			console.log(id + ' não encontrado');
+			res.status(404)
+			var err new Error('Não Encontrado');
+			err.status = 404;
+			res.format({
+				html: function(){
+					next(err);					
+				},
+				json: function(){
+					res.json({message: err.status + ' ' + err});
+				}
+			});
+		}else{
+			req.id = id;
+			next();
+		}
 	});
+});
 
-	router.route('/:id').get(function(req, res){
+router.route('/:id').get(function(req, res){
+	mongoose.model('Blob').findById(req.id, function(err, blob){
+		if(err){
+			console.log('GET Error: ' + err);
+		}else{
+			console.log('GET: ' + blob._id);
+			var blobdob = blob.dob.toISOString();
+			blobdob = blobdob.substring(0, blobdob.indexOf('T'))
+			res.format({
+				html: function(){
+					res.render('blobs/show', {
+						"blobdob": blobdob,
+						"blob" : blob
+					});
+				},
+				json: function(){
+					res.json(blob);
+				}
+			});
+		}
+	});
+});
+
+router.get('/:id/edit', function(req, res){
 		mongoose.model('Blob').findById(req.id, function(err, blob){
-			if(err){
-				console.log('GET Error: ' + err);
+			if(err)	{
+				console.log('GET Error: ' + err)
 			}else{
 				console.log('GET: ' + blob._id);
+
 				var blobdob = blob.dob.toISOString();
 				blobdob = blobdob.substring(0, blobdob.indexOf('T'))
 				res.format({
 					html: function(){
-						res.render('blobs/show', {
+						res.render('blobs/edit', {
 							"blobdob": blobdob,
-							"blob" : blob
+							"blob": blob
 						});
 					},
 					json: function(){
@@ -114,3 +138,66 @@ router.route('/')
 			}
 		});
 	});
+
+router.put('/:id/edit', function(req, res){
+	var name = req.body.name;
+	var badge = req.body.badge;
+	var dob = req.body.dob;
+	var company = req.body.company;
+	var isloved = req.body.isloved;
+
+	//primeiro encontra o arquivo no Mongo e depois atualiza com os novos valores
+	mongoose.model('Blob').findById(req.id, function(err, blob){
+		blob.update({
+			name: name,
+			badge: badge,
+			dob: dob,
+			company: compnay,
+			isloved: isloved
+		}, function(err, blobID){
+			if(err){
+				res.send('Deu ruim no UPDATE');
+			}else{
+				res.format({
+					html: function(){
+						res.redirect("/blobs/" + blob._id);
+					},
+					json: function(){
+						res.json(blob);
+					}
+				});
+			}
+		});
+	});
+
+});
+
+router.delete('/:id/edit', function(req, res){
+	mongoose.model('Blob').findById(req.id, function(err, blob){
+		if(err)
+		{
+			res.send('Deu ruim: ' + err);
+		}else{
+			blob.remove(function(err, blob){
+				if(err){
+					res.send('Deu ruim de novo: ' + err);
+				}else{
+					res.format({
+						html: function(){
+							res.redirect('/blobs');
+						},
+						json: function(){
+							res.json({
+										message: 'deletado', 
+										item: blob
+									});
+						}
+					});
+				}
+
+			});
+		}
+	});
+});
+
+module.exports = router;
